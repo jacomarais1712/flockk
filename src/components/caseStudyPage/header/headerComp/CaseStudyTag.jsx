30fa30492fb151d9e45b87c2cd593ad7275818cf
Jaco Marais <jaco@remotech.co.za>
2025-07-30 10:10:00 +0200

    final commit for mobile layout changes

--- src/components/caseStudyPage/header/headerComp/CaseStudyTag.jsx
+++ src/components/caseStudyPage/header/headerComp/CaseStudyTag.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function CaseStudyTag() {
   return (
-    <div className="flex relative gap-2 justify-center items-center px-4 py-1.5 h-8 rounded bg-zinc-900 w-fit">
+    <div className="flex relative gap-2 justify-center items-center px-4 py-1.5 h-8 rounded bg-zinc-900 w-fit max-sm:self-center">
       <span className="relative text-xs font-bold tracking-wide leading-5 text-center text-emerald-400 uppercase">
         Case Study
       </span>
